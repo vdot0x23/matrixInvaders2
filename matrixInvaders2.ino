5721e64a3764a18479b82306db83aa7909e6999c
Victor Büttner <vbuttner@h493.aau1x-3.klient.audt.site.aau.dk>
2018-11-07 16:28:32 +0100

    check

--- matrixInvaders2.ino
+++ matrixInvaders2.ino
@@ -1,6 +1,5 @@
 #include "LedControl.h"
 
-
 //LED CONTROL
 int dinPin = 12;
 int clkPin = 10;
@@ -8,15 +7,20 @@ int csPin = 11;
 int ledPanels = 1;
 LedControl lc = LedControl(dinPin, clkPin, csPin, ledPanels);
 
-//BUTTON PINS
-int lButPin = 2;
-int rButPin = 3;
-
 /////////////////////
 //   GAME BOARD   //
 ///////////////////
 
-
+byte rainDrops[] = {
+  B00000001,
+  B00000010,
+  B00000100,
+  B00001000,
+  B00010000,
+  B00100000,
+  B01000000,
+  B10000000,
+};
 byte maxByte = B11111111;
 byte player = B00010000;
 byte board[] = {
@@ -30,15 +34,16 @@ byte board[] = {
   player ^ maxByte,
 };
 
+
 void rain() {
   if (board[0] == 0) {
-    board[0] = rngByte();
+    board[0] = rainDrops[random(0, 8)];
     return;
   }
   else {
     for (int i = 5; i >= 0; i--) {
       board[i + 1] = board[i];
-      delay(100);
+      delay(22);
       board[i] = B00000000;
     }
     return;
@@ -51,9 +56,14 @@ void draw() {
   }
 }
 
-byte rngByte() {
-  byte aByte = random(0, 255) & random(0, 255) & random(0, 255);
-  return aByte;
+void check() {
+  if ((board[7] | board[6]) == 255) {
+    //Serial.println("you loose");
+    return;
+  }
+  else {
+    return;
+  }
 }
 
 /////////////////////////
@@ -61,30 +71,60 @@ byte rngByte() {
 ///////////////////////
 
 /*
-  void lShift() {
-  if (debounce() == true && board[7] != 128) {
-    board[7]
+ 
 
-  }
-  }
-*/
+volatile unsigned long lastDebounce = 0;
 
+int debounce() {
+  int debounceCutoff = 111;
+  if ((long)millis() - lastDebounce >= debounceCutoff) {
+    lastDebounce = millis();
+    return 1;
+  }
+  else {
+    return 0;
+  }
+}
+
+int lButPin = 2;
+int rButPin = 3;
+
+void lShift() {
+  if (player < 128 && debounce() == 1) {
+    player = player << 1;
+    board[7] = player ^ maxByte;
+    draw();
+  }
+}
+
+void rShift() {
+  if (player > 1 && debounce() == 1) {
+    player = player >> 1;
+    board[7] = player ^ maxByte;
+    draw();
+  }
+}
+
+ */
 
 //CODE IN setup() GETS RUN ONCE
 void setup() {
   //Serial communication
   Serial.begin(9600);
 
-  //RANDOM
   randomSeed(analogRead(A0));
 
-  /*
-    //Interrupts
-    pinMode(lButPin, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(lButPin), lShift, RISING);
-    pinMode(rButPin, INPUT_PULLUP);
-    attachInterrupt(digitalPinToInterrupt(rButPin), rShift, RISING);
-  */
+  //Input interrupts
+
+/*
+  
+  pinMode(lButPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(lButPin), lShift, FALLING);
+  pinMode(rButPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(rButPin), rShift, FALLING);
+
+*/
+
   //Turn on all attached displays
   for (int i = 0; i < lc.getDeviceCount(); i++) {
     lc.shutdown(i, false);
@@ -98,7 +138,7 @@ void loop() {
   interrupts();
   rain();
   draw();
-  noInterrupts();
-  //check();
+  //noInterrupts();
+  check();
 
 }

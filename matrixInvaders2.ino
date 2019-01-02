6a25336c94bdeab41ebb3a2ea53c84657da97323
vdot0x23 <vdot0x23@localhost.localdomain>
2019-01-02 19:24:57 +0100

    all done, could be polished

--- matrixInvaders2.ino
+++ matrixInvaders2.ino
@@ -1,17 +1,20 @@
 #include "LedControl.h"
 
+//RESET GAME
+void (*resetGame) (void) = 0;
+
 //LED CONTROL
 int dinPin = 12;
 int clkPin = 10;
 int csPin = 11;
 int ledPanels = 1;
+
 LedControl lc = LedControl(dinPin, clkPin, csPin, ledPanels);
 
 /////////////////////
 //   GAME BOARD   //
 ///////////////////
 
-//Generer 1 og bitshift randomly
 byte rainDrops[] = {
   B00000001,
   B00000010,
@@ -22,8 +25,10 @@ byte rainDrops[] = {
   B01000000,
   B10000000,
 };
+
 byte maxByte = B11111111;
 byte player = B00010000;
+
 byte board[] = {
   B00000000,
   B00000000,
@@ -45,7 +50,7 @@ void genDrops() {
   lastRandom = board[0];
 }
 
-float rainRate = 250;
+float rainRate = 500;
 unsigned long long rainRateTimer = 0;
 
 void rain() {
@@ -63,25 +68,53 @@ void rain() {
   }
 }
 
-void draw() {
-  for (int i = 0; i < 8; i++) {
-    lc.setRow(0, i, board[i]);
-  }
-}
-
-void check() {
-  if ((board[7] | board[6]) == 255) {
-    //Serial.println("you loose");
-  }
-}
-
 int difficultyRate = 100;
 unsigned long long difficultyRateTimer = 0;
+
+float initDifficulty(int x) {
+  float a = rainRate;
+  for (int i = 0; i < x; i++) {
+    a *= 0.999;
+  }
+  return (a);
+}
 
 void difficulty() {
   if (difficultyRate < millis() - difficultyRateTimer) {
     difficultyRateTimer = millis();
     rainRate *= 0.999;
+  }
+}
+
+void draw(byte a[]) {
+  for (int i = 0; i < 8; i++) {
+    lc.setRow(0, i, a[i]);
+  }
+}
+
+void check() {
+  byte emptyScreen[] = {
+    B00000000,
+    B00000000,
+    B00000000,
+    B00000000,
+    B00000000,
+    B00000000,
+    B00000000,
+    B00000000,
+  };
+
+  if ((board[7] | board[6]) == 255) {
+    for (int i = 0; i < 3; i++) {
+      draw(board);
+      delay(300);
+      draw(emptyScreen);
+      delay(300);
+    }
+    Serial.print("Your score was: ");
+    Serial.println(500 - rainRate);
+    delay(100);
+    resetGame();
   }
 }
 
@@ -120,6 +153,99 @@ void inputCheck() {
   }
 }
 
+////////////////////
+//   START MENU  //
+//////////////////
+
+float startMenuTime = 5000;
+unsigned long long menuTimer = 0;
+
+int startInput(int dSelect) {
+  if (startMenuTime < millis() - menuTimer) {
+    menuTimer = millis();
+    dSelect = 4;
+  }
+  else if (digitalRead(lButPin) == LOW && dSelect > 1 && inputRate < millis() - inputRateTimer) {
+    inputRateTimer = millis();
+    dSelect--;
+  }
+  else if (digitalRead(rButPin) == LOW && dSelect < 3 && inputRate < millis() - inputRateTimer) {
+    inputRateTimer = millis();
+    dSelect++;
+  }
+  return (dSelect);
+}
+
+void start() {
+
+  byte startScreen1[] = {
+    B00000000,
+    B00111000,
+    B01011000,
+    B00011000,
+    B00011000,
+    B00011000,
+    B00011000,
+    B00111100,
+  };
+
+  byte startScreen2[] = {
+    B00000000,
+    B00111000,
+    B01000100,
+    B00000100,
+    B00001000,
+    B00010000,
+    B00100000,
+    B01111110,
+  };
+
+  byte startScreen3[] = {
+    B00000000,
+    B01111000,
+    B00000100,
+    B00000100,
+    B01111000,
+    B00000100,
+    B00000100,
+    B01111000,
+  };
+
+  struct level {
+    int easy;
+    int medium;
+    int hard;
+  } level1;
+
+  level1.easy = 0;
+  level1.medium = 300;
+  level1.hard = 600;
+
+  int dSelect = 1;
+  float tempRainRate = 0;
+
+  while (dSelect < 4) {
+    switch (dSelect) {
+      case 1:
+        draw(startScreen1);
+        tempRainRate = initDifficulty(level1.easy);
+        break;
+
+      case 2:
+        draw(startScreen2);
+        tempRainRate = initDifficulty(level1.medium);
+        break;
+
+      case 3:
+        draw(startScreen3);
+        tempRainRate = initDifficulty(level1.hard);
+        break;
+    }
+    dSelect = startInput(dSelect);
+  }
+  rainRate = tempRainRate;
+}
+
 //CODE IN setup() GETS RUN ONCE
 void setup() {
   //Serial communication
@@ -137,13 +263,14 @@ void setup() {
     lc.setIntensity(i, 1);
     lc.clearDisplay(i);
   }
+  start();
 }
 
 //CODE IN loop() GETS RUN REPEATEDLY
 void loop() {
   rain();
   inputCheck();
-  draw();
+  draw(board);
   check();
   difficulty();
 }
